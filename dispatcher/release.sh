32486d2328ae152641ee7c65b43a6f177f4e0dcc
Steve Mansour <sman@stevemansour.com>
2024-07-25 12:59:16 -0700

    WIP

---
+++ dispatcher/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#-----------------------------------
+# Shut down the dispatcher service
+#-----------------------------------
+sudo systemctl stop plato.dispatcher.service
+
+#-----------------------------------------------------------
+# Copy the updated dispatcher files to the release location
+#-----------------------------------------------------------
+cp dispatcher /usr/local/simq/dispatcher/
+
+#-----------------------------------
+# Start the dispatcher service
+#-----------------------------------
+sudo systemctl start plato.dispatcher.service
